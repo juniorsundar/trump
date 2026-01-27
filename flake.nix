425e0fbe1b9e22c405002fb98d5202ea82405180
Junior Sundar <juniorsundar@gmail.com>
2026-01-27 23:58:36 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,74 +1,87 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    fenix,
-    nixpkgs,
-  }: let
-    # Define supported systems
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-
-    # Helper function to generate outputs for each system
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-    # Define the Rust toolchain components
-    rustToolchainComponents = [
-      "cargo"
-      "clippy"
-      "rust-src"
-      "rustc"
-      "rustfmt"
-      "rust-analyzer"
-    ];
-  in {
-    # Development shells for each supported system
-    devShells = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      rustToolchain = fenix.packages.${system}.combine [
-        (fenix.packages.${system}.complete.withComponents rustToolchainComponents)
-        fenix.packages.${system}.targets.wasm32-unknown-unknown.latest.rust-std
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
-    in {
-      default = pkgs.mkShell {
-        name = "rust-dev-shell";
-        packages = [
-          rustToolchain
-          pkgs.uv
-          pkgs.python3
-          pkgs.wasm-pack
-          pkgs.nodejs
-          pkgs.openssl
-          pkgs.libssh2
-        ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.openssl pkgs.libssh2 ]}:$LD_LIBRARY_PATH
-          
-          # Helps compilation find headers
-          export OPENSSL_DIR="${pkgs.openssl.dev}"
-          export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+          # Use the Rust toolchain from fenix
+          toolchain = fenix.packages.${system}.stable.toolchain;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in
+        {
+          default = rustPlatform.buildRustPackage {
+            pname = manifest.name;
+            version = manifest.version;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
 
-          VENV_DIR=".venv"
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
 
-          if [ ! -d "$VENV_DIR" ]; then
-            echo "Creating Python virtual environment at $VENV_DIR..."
-            uv venv $VENV_DIR -p ${pkgs.python3}/bin/python
-          fi
+            buildInputs = [
+              pkgs.openssl
+              pkgs.libssh2
+              pkgs.zlib
+            ];
+          };
+        }
+      );
 
-          source "$VENV_DIR/bin/activate"
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          toolchain = fenix.packages.${system}.complete.toolchain;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              toolchain
+              pkgs.pkg-config
+              pkgs.openssl
+              pkgs.libssh2
+              pkgs.zlib
+            ];
 
-          uv pip install pre-commit
-          pre-commit install
-          # zsh
-        '';
-      };
-    });
-  };
+            shellHook = ''
+              export LD_LIBRARY_PATH=${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.openssl
+                  pkgs.libssh2
+                  pkgs.zlib
+                ]
+              }:$LD_LIBRARY_PATH
+            '';
+          };
+        }
+      );
+    };
 }
